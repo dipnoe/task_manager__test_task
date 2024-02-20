4abfeb48c58c1e52796aededf9facafb6150eead
Vagner Elena <dipnoe@gmail.com>
2024-02-20 22:04:46 +0700

    feature: add create view

--- src/task_manager/views.py
+++ src/task_manager/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+
+from src.task_manager.models import Task
+from src.task_manager.serializers import TaskSerializer
+
 
 # Create your views here.
+class TaskCreateAPIView(CreateAPIView):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
