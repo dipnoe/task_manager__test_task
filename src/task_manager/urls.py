e08a463a4a37f1d596e005965e0215eeb5352fb7
Vagner Elena <dipnoe@gmail.com>
2024-02-20 23:51:17 +0700

    feature: add drf yasg

--- src/task_manager/urls.py
+++ src/task_manager/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from src.task_manager.apps import TaskManagerConfig
-from src.task_manager.views import TaskCreateAPIView
+from task_manager.apps import TaskManagerConfig
+from task_manager.views import TaskCreateAPIView, TaskUpdateAPIView, TaskDestroyAPIView
 
 app_name = TaskManagerConfig.name
 
 urlpatterns = [
     path('create/', TaskCreateAPIView.as_view(), name='task_create'),
+    path('update/<int:pk>/', TaskUpdateAPIView.as_view(), name='task_update'),
+    path('destroy/<int:pk>/', TaskDestroyAPIView.as_view(), name='task_destroy'),
 ]
