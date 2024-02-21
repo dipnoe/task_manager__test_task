289d02fee298dbc24c5a8baa87789ddd8314cbf9
Vagner Elena <dipnoe@gmail.com>
2024-02-21 21:04:47 +0700

    feature: add admin page for Task and Executor models

--- src/task_manager/admin.py
+++ src/task_manager/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from task_manager.models import Task, Executor
+
+
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Task._meta.get_fields()]
+
+
+@admin.register(Executor)
+class ExecutorAdmin(admin.ModelAdmin):
+    list_display = ['name',]
